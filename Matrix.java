63a5dce1681fd139e9c3e3bc8dc3355ef0958729
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-09 21:38:26 +0000

    added setElement

--- Matrix.java
+++ Matrix.java
@@ -7,8 +7,19 @@ public class Matrix {
 		for (xCount = 0; xCount < x; xCount++){
 			int yCount = 0;
 			for (yCount = 0; yCount < y; yCount++) {
-					matrix[x][y] = 1;
+					this.matrix[x][y] = 1;
 			}	
 		}
 	}
+
+	public void setElement(int x, int y, int value) {
+		int xLength = this.matrix.length;
+		int yLength = this.matrix[x].length;
+		if (xLength < x || yLength < y) {
+			System.out.println("Not a valid array index.");
+		} else {
+			this.matrix[x][y] = value;
+		}
+	}
+			
 }
