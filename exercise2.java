e69b3be59cd45edb11f74db19332e30d333b5459
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-09 19:11:59 +0000

    first commit

---
+++ exercise2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class exercise2 {
+	public static void main(String[] args) {
+
+		System.out.print("How long is your first array? ");
+		Scanner sc = new Scanner(System.in);
+		int arrayLength = sc.nextInt();
+		int[] src;
+		src = new int[arrayLength];
+		int count = 0;
+		while ( count < arrayLength) {
+			System.out.print("Enter a number: ");
+			sc = new Scanner(System.in);
+			src[count] = sc.nextInt();
+			count++;
+		}
+
+		System.out.print("How long is your second array? ");
+		sc = new Scanner(System.in);
+		arrayLength = sc.nextInt();
+		int[] dst;
+		dst = new int[arrayLength];
+		count = 0;
+		while ( count < arrayLength) {
+			System.out.print("Enter a number: ");
+			sc = new Scanner(System.in);
+			dst[count] = sc.nextInt();
+			count++;
+		}
+		
+
+		
+	}
+}
