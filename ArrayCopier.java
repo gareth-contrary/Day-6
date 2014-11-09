c1323c0003320de2306356d6f87e161ae4aade6c
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-09 19:11:18 +0000

    first commit

---
+++ ArrayCopier.java
@@ -0,0 +1,27 @@
+public class ArrayCopier {
+
+	private int[] src;
+	private int[] dst;
+
+	public ArrayCopier(int[] src, int[] dst) {
+		this.src = src;
+		this.dst = dst;
+	}
+
+	public void copy(int[] src, int[] dst) {
+		int srcLength = src.length;
+		int dstLength = dst.length;
+		int count = 0;
+		if (src.length <= dst.length) {
+			for(count = 0; count < src.length; count++) {
+				dst[count] = src[count];
+			}
+		} else {
+			for(count = 0; count < dst.length; count++) {
+				if (count > src[count]) { dst[count] = 0; }
+				else { dst[count] = src[count]; }
+			}
+		}
+	}
+
+}
